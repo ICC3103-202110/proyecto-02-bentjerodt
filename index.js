1f89ffcba9169e70301c1d75637fdc1b498ee0d6
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2021-06-18 11:16:27 -0400

    node

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//We import some parte of the code and some modules 
 const {initModel} = require("./model");
 const {updateModel} = require("./update");
 const {view} = require("./view");
 const {app} = require("./app");
 
+//The state contains the model (our tempertures) and the function view, that print these variables
 const state = {
     model: initModel,
     currentView: view(initModel)
 }
 
+//we execute the app
 app(state,updateModel,view);
